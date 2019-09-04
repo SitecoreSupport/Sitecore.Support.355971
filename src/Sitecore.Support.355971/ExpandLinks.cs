4924840bc14a06f412d5a3b02bd36ec1ef0e195c
DK\brk <brk@sitecore.net>
2019-09-04 14:21:41 -0400

    Initial Commit

---
+++ src/Sitecore.Support.355971/ExpandLinks.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore;
+using Sitecore.Configuration;
+using Sitecore.Diagnostics;
+using Sitecore.Links;
+using Sitecore.Pipelines.RenderField;
+using Sitecore.Web;
+
+namespace Sitecore.Support.Pipelines.RenderField
+{
+    public class ExpandLinks
+    {
+        public virtual void Process(RenderFieldArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            if (!Context.PageMode.IsExperienceEditorEditing)
+            {
+                try
+                {
+                    args.Result.FirstPart = DynamicLink.ExpandLinks(args.Result.FirstPart, Settings.Rendering.SiteResolving);
+                    args.Result.LastPart = DynamicLink.ExpandLinks(args.Result.LastPart, Settings.Rendering.SiteResolving);
+
+                }
+                catch (InvalidLinkFormatException ex)
+                {
+                    Log.Error("Failed to render ill-formatted link", ex, this);
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.355971/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.355971")]
+[assembly: AssemblyProduct("Sitecore.Support.355971")]
+[assembly: ComVisible(false)]
